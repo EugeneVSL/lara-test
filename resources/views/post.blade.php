8527a4b2583293d63110eb28dcbf9e4767a21cf6
Eugene <eugenevasile@gmail.com>
2023-07-10 12:12:39 +0200

    Switched to Blade syntax and layouts
    Switched to Blade syntax and layouts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post</title>
+@extends('layout')
 
-    <link rel="stylesheet" href="../css/app.css">
-</head>
-<body>
+@section('content')
 
     <article>
 
@@ -18,9 +10,8 @@
     </article>
 
     <a href="/lara-test/">Go Back</a>
+    
+@endsection
 
-    <script src="../js/script.js"></script>
 
-</body>
-
-</html>
+    
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@extends('layout')
 
-    <link rel="stylesheet" href="../css/app.css">
-</head>
-<body>
+@section('content')
 
     @foreach ($posts as $post)
 
@@ -20,9 +12,5 @@
         </article>
 
     @endforeach;
-
-    <script src="../js/script.js"></script>
-
-</body>
-
-</html>
+        
+@endsection
